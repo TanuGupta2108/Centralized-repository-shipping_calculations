a31c0742c0ff207ada2e31319328f7e7e45cd990
Tanu Gupta <guptatanu090@gmail.com>
2026-06-24 17:24:08 +0530

    Revert "anaother line added"
    This reverts commit 500b48c39d24fea70e2497ca6263fad9d8ca228d.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,6 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by Tanu Gupta
- # Here is another update by Tanu Gupta
-
 
